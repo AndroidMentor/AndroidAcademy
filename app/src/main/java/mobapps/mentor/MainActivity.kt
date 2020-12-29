511002e8c423cd2011510a0699f34b4a9931be61
«Timurovic» <ttimurovic@gmail.com>
2020-12-30 00:54:48 +0300

    first push

---
+++ app/src/main/java/mobapps/mentor/MainActivity.kt
@@ -0,0 +1,11 @@
+package mobapps.mentor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
