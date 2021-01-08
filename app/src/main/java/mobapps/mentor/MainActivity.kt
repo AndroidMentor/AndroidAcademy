2d611ca0c81f5d6177829e4a0cc8c4af07ca072f
«Timurovic» <ttimurovic@gmail.com>
2021-01-09 02:32:20 +0300

    добавил реализацию домашнего задания №2

--- app/src/main/java/mobapps/mentor/MainActivity.kt
+++ app/src/main/java/mobapps/mentor/MainActivity.kt
@@ -1,11 +1,18 @@
 package mobapps.mentor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.setOnClickListener {
+            val intent = Intent(this, MovieDetailsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
